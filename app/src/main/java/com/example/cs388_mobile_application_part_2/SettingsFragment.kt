c59180b10a0ecefdf8e0098aa7749ff4120b231a
Guanhe Fu <f1016142506@gmail.com>
2026-04-09 11:14:13 -0400

    Implement SettingsFragment and clean up Fragment boilerplate
    - Create `SettingsFragment` and its corresponding layout `fragment_settings.xml`.
    - Remove unused boilerplate code, parameters, and factory methods from `UpcomingFragment` and `ListFragment`.

--- app/src/main/java/com/example/cs388_mobile_application_part_2/SettingsFragment.kt
+++ app/src/main/java/com/example/cs388_mobile_application_part_2/SettingsFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class UpcomingFragment : Fragment() {
+class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
