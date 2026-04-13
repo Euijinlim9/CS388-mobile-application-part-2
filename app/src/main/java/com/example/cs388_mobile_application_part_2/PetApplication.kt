3f2afd224f72f210594a2f471cfd550191a69d53
Guanhe Fu <f1016142506@gmail.com>
2026-04-13 13:08:49 -0400

    Merge jimmy-dev

--- app/src/main/java/com/example/cs388_mobile_application_part_2/PetApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cs388_mobile_application_part_2;
-
-import android.app.Application;
-
-class PetApplication: Application() {
-    val db by lazy{AppDatabase.getInstance(this)}
-}
